ecbaa56b56873a8aa99550f474b93eb0c43f0861
Zhiqi Li <Zhiqi.L@arbin.com>
2025-01-29 14:46:54 -0600

    [feat]add on examples

--- backend/examples/call_login_api.py
+++ backend/examples/call_login_api.py
@@ -1,28 +1,41 @@
 import requests
 
-BASE_URL = "http://127.0.0.1:8000"
 
+# ============================================================
+# IMPORTANT: Ensure that FastAPI is running before executing this code!
+# ============================================================
 
-def login():
-    url = f"{BASE_URL}/login"
-    payload = {
-        "username": "admin",
-        "password": "000000",
-        "ipadress": "127.0.0.1",
-    }
+def login(request_payload):
+    """
+    Perform user login.
+    :param request_payload: A dictionary containing the login credentials.
+                            Expected keys: "username", "password", "ipaddress" and "port"(optional).
+
+    :return: print the feedback login.
+    """
+    ip_address = request_payload.get("ipaddress", "127.0.0.1")
+    port = 8000  # Must match the port where FastAPI is running
+    base_url = f"http://{ip_address}:{port}"
+    login_url = f"{base_url}/login"
+
     try:
-        response = requests.post(url, json=payload)
+        response = requests.post(login_url, json=request_payload)
         response_data = response.json()
 
         if response_data["success"]:
             print("Login successfully.")
         else:
-            print("Login failed.")
+            print(f"Login failed. Error: {response_data["error"]}")
 
         print(response_data)
-    except requests.exceptions.RequestException as e:
-        print("API Request Error:", e)
+    except Exception as e:
+        print("An Unexpected Error Occurred: ", e)
 
 
 if __name__ == "__main__":
-    login()
+    payload = {
+        "username": "admin",
+        "password": "000000",
+        "ipaddress": "127.0.0.1"
+    }
+    login(payload)
